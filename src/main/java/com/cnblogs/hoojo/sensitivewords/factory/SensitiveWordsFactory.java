33a0b75a0314252b2fdf430ba75b697d94d0aae4
hoojo <hoojo@qq.com>
2018-04-17 12:05:55 +0800

    :sparkles: :bento: :recycle:  Changed 敏感词库过滤实现工厂

--- src/main/java/com/cnblogs/hoojo/sensitivewords/factory/SensitiveWordsFactory.java
+++ src/main/java/com/cnblogs/hoojo/sensitivewords/factory/SensitiveWordsFactory.java
@@ -3,7 +3,7 @@ package com.cnblogs.hoojo.sensitivewords.factory;
 import com.cnblogs.hoojo.sensitivewords.filter.AbstractSensitiveWordsFilter;
 
 /**
- * 敏感词库工程类
+ * 敏感词库过滤实现工厂
  * @author hoojo
  * @createDate 2018年2月2日 下午4:05:29
  * @file SWFacotry.java
